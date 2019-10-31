cf7a7ca5eeac673e1c59dfe9c04294bcf8099d45
Nhan Nguyen <nhan_nguyen@brangista.com>
2019-10-31 18:14:09 +0900

    update template

--- main.go
+++ main.go
@@ -2,18 +2,18 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
+	"github.com/gorilla/mux"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
-	"bytes"
-	"github.com/gorilla/mux"
-	"html/template"
 	"strconv"
-	 "strings"
+	"strings"
 	"time"
 )
 
@@ -87,8 +87,8 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		fResult, err := ioutil.TempFile("/tmp", "encoded")
 		checkError("Can't create tmp file", err)
 		defer os.Remove(fResult.Name())
-		
-		for _,item := range ret {
+
+		for _, item := range ret {
 			fResult.WriteString(item + "\n")
 		}
 
@@ -102,44 +102,36 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
-func UrlEncoded(str string) (string, error) {
-    u, err := url.Parse(str)
-    if err != nil {
-        return "", err
-    }
-    return u.String(), nil
-}
 func checkError(message string, err error) {
 	if err != nil {
 		log.Fatal(message, err)
 	}
 }
 
-
-func urlencode(s string) (result string){
-	for _, c := range(s) {
-	  if c <= 0x7f { // single byte 
-		result += fmt.Sprintf("%%%X", c)
-	  } else if c > 0x1fffff {// quaternary byte
-		result += fmt.Sprintf("%%%X%%%X%%%X%%%X",
-		  0xf0 + ((c & 0x1c0000) >> 18),
-		  0x80 + ((c & 0x3f000) >> 12),
-		  0x80 + ((c & 0xfc0) >> 6),
-		  0x80 + (c & 0x3f),
-		)
-	  } else if c > 0x7ff { // triple byte
-		result += fmt.Sprintf("%%%X%%%X%%%X",
-		  0xe0 + ((c & 0xf000) >> 12),
-		  0x80 + ((c & 0xfc0) >> 6),
-		  0x80 + (c & 0x3f),
-		)
-	  } else { // double byte
-		result += fmt.Sprintf("%%%X%%%X",
-		  0xc0 + ((c & 0x7c0) >> 6),
-		  0x80 + (c & 0x3f),
-		)
-	  }
+func urlencode(s string) (result string) {
+	for _, c := range s {
+		if c <= 0x7f { // single byte
+			result += fmt.Sprintf("%%%X", c)
+		} else if c > 0x1fffff { // quaternary byte
+			result += fmt.Sprintf("%%%X%%%X%%%X%%%X",
+				0xf0+((c&0x1c0000)>>18),
+				0x80+((c&0x3f000)>>12),
+				0x80+((c&0xfc0)>>6),
+				0x80+(c&0x3f),
+			)
+		} else if c > 0x7ff { // triple byte
+			result += fmt.Sprintf("%%%X%%%X%%%X",
+				0xe0+((c&0xf000)>>12),
+				0x80+((c&0xfc0)>>6),
+				0x80+(c&0x3f),
+			)
+		} else { // double byte
+			result += fmt.Sprintf("%%%X%%%X",
+				0xc0+((c&0x7c0)>>6),
+				0x80+(c&0x3f),
+			)
+		}
 	}
-  
+
 	return result
-  }
+}
